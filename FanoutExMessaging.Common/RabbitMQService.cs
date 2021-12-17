c060641a708e82013126c0136e6abb3f1dac8d3c
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-17 22:48:12 +0300

    FanoutExMessaging.Common kütüphanesi eklendi

---
+++ FanoutExMessaging.Common/RabbitMQService.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanoutExMessaging.Common
+{
+    public class RabbitMQService
+    {
+        private string _hostName = "localhost";
+
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
