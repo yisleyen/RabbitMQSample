8f8ba67c66a95a2a1197f63feb24f2f1c77f6464
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-17 23:13:14 +0300

    FanoutExMessaging.ShippingConsumer konsol uygualaması eklendi

---
+++ FanoutExMessaging.ShippingConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using FanoutExMessaging.Common;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace FanoutExMessaging.ShippingConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rabbitMQService = new RabbitMQService();
+
+            using (var connnection = rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connnection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.Span;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("foo.shipping üzerinden mesaj alındı: {0}", message);
+                    };
+
+                    channel.BasicConsume("foo.shipping", false, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
